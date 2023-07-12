0f53405067baebf4fbd9ef042177a2de097aae2d
deepikark8 <deepikark8@gmail.com>
2023-07-11 22:38:49 -0700

    selenium grid

--- SeleniumGrid/src/test/java/GoogleTest.java
+++ SeleniumGrid/src/test/java/GoogleTest.java
@@ -1,32 +1,35 @@
-import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 
 public class GoogleTest {
-	
+
 	@Test
 	public void HomePageCheck() throws MalformedURLException {
+
+
 		
+
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setBrowserName("chrome");
-		
-		
+		//cap.setPlatform(Platform.LINUX);
+		// set the timeout to a maximum of 300 seconds
+		//cap.setCapability("browserstack.idleTimeout", "300");
+
 		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.86.250:4444"),cap);
 		driver.get("https://www.google.com/");
 		driver.findElement(By.name("q")).sendKeys("deepika ramkumar");
 		System.out.println(driver.getTitle());
-		
-		
+
+
 	}
-	
+
 
 }
